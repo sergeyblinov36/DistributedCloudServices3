c99e10403ab917a0701fabc71b0d16c538331c09
sergey <sergeyblinov36@gmail.com>
2019-12-26 14:54:57 +0200

    done

---
+++ assets/modules/router.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+const tvGuideController = require('./controller');
+const dotenv = require ('dotenv');
+dotenv.config();
+
+const tvGuideRouter = new Router();
+
+tvGuideRouter.get(`/date/:Date`, tvGuideController.getByTime);
+tvGuideRouter.get(`/name/:id`, tvGuideController.getById);
+tvGuideRouter.get("/all", tvGuideController.getAll);
+//tvGuideRouter.get("/*",tvGuideController.error);
+tvGuideRouter.post('/show', tvGuideController.createShow);
+tvGuideRouter.put('/show/:id', tvGuideController.updateShow);
+tvGuideRouter.delete('/show/:id', tvGuideController.deleteShow);
+
+module.exports = {
+    tvGuideRouter
+};
+
+
